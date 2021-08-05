8301a3f86ac9689f8a3920ee8480f4897cbe8318
deutho <59829810+deutho@users.noreply.github.com>
2021-08-05 21:47:18 +0200

    Cards are done

--- DerDieDaz/src/app/features/info-tile/info-tile.component.ts
+++ DerDieDaz/src/app/features/info-tile/info-tile.component.ts
@@ -6,11 +6,65 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./info-tile.component.css']
 })
 export class InfoTileComponent implements OnInit {
-  @Input() public text: string;
+  @Input() public header: string;
+  @Input() public image: string;
+  @Input() public descriptionFront: string;
+  @Input() public descriptionBack: string;
+  @Input() public buttonText: string;
+  @Input() public buttonAction: string;
+  loaded = false
+  height = 0;
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    this.loaded = false
+  }
+
+  // calc height of cards by Typescript because CSS can't do it...
+  callSizeUpdate(){
+    if (this.loaded == false){
+     
+      this.addHeight(document.getElementById(this.header + this.image + 'lable'))     
+      this.loaded = true
+      if(this.image == undefined) this.height -= 190;
+      document.getElementById(this.header + this.image + 'lable').style.height = this.height + 100 + "px"
+      this.makeAllCardsSameHeight()
+    }    
+  }
+
+  addHeight(element){
+    if(element.className == "flip-card-back") return
+    this.height += element.getBoundingClientRect().height
+    var children = element.children 
+    for(var i = 0; i < children.length; i++){
+      this.addHeight(children[i])
+    }    
+  }
+
+  // makes all cards the same height
+  makeAllCardsSameHeight(){
+    var maxHeight = 0;
+    var allCards = document.getElementsByClassName("card-height")
+    for(var i = 0; i<allCards.length; i++){
+      if (maxHeight < allCards[i].getBoundingClientRect().height){
+        maxHeight = allCards[i].getBoundingClientRect().height
+      }
+    }
+
+    for(var j = 0; j<allCards.length; j++){
+      if(document.getElementById(allCards[j].id) != undefined) document.getElementById(allCards[j].id).style.height = maxHeight + "px"
+    }
+  }
+  
+  buttonClick(buttonAction){
+    if(buttonAction =="print") {
+      console.log("click")
+      console.log(this.header)
+      
+    }
   }
 
 }
