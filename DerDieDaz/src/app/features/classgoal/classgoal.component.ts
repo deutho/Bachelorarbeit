e233ebc9d814dc7aa8e6ce640b06feb9877ec5c3
deutho <59829810+deutho@users.noreply.github.com>
2021-08-09 19:11:48 +0200

    working on getting teacher

--- DerDieDaz/src/app/features/classgoal/classgoal.component.ts
+++ DerDieDaz/src/app/features/classgoal/classgoal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Teacher } from 'src/app/models/users.model';
 import { AppService } from 'src/app/services/app.service';
 import { FirestoreDataService } from 'src/app/services/firestore-data.service';
@@ -8,19 +8,41 @@ import { FirestoreDataService } from 'src/app/services/firestore-data.service';
   templateUrl: './classgoal.component.html',
   styleUrls: ['./classgoal.component.css']
 })
-export class ClassgoalComponent implements OnInit {
+export class ClassgoalComponent implements OnInit, OnDestroy {
 
+  currentUserSubscription;
   currentUser;
-  teacher: Teacher;
+  teacher;
+  goal: string;
 
   constructor(private app: AppService, private afs: FirestoreDataService) { }
 
+
   async ngOnInit() {
     this.app.myHeader("Klassenziel")
-    this.currentUser = this.afs.getUser
-    console.log(this.currentUser)
-    this.teacher = await this.afs.getUserPerID(this.currentUser.parent)
-    console.log(this.teacher[0])
+    this.currentUserSubscription = this.afs.currentUserStatus.subscribe(data => {
+      this.currentUser = data
+      if(data != null){
+        this.initialize()
+      }
+    })   
+  
+  }
+
+  async initialize(){
+    this.teacher = (await this.afs.getUserPerID(this.currentUser.parent))[0]
+    // this.teacher.classtargets.forEach((value, key) => {
+    //   console.log(key + " " + value)
+    // })
+    // console.log(this.teacher.classtargets.size)
+    // for(let key of this.teacher.classtargets.keys()) {
+    //   console.log(key)
+    // }
+    console.log(this.teacher.classtargets.keys())
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 
 }
