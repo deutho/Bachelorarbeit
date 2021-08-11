ef083e5688b01815b81d2a6147c1df2605cde13b
stefan-hinterhoelzl <59830330+stefan-hinterhoelzl@users.noreply.github.com>
2021-08-11 15:58:38 +0200

    buying rewards

--- DerDieDaz/src/app/features/shop/shop.component.ts
+++ DerDieDaz/src/app/features/shop/shop.component.ts
@@ -1,29 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ObjectUnsubscribedError } from 'rxjs';
+import { Purchase } from 'src/app/models/purchase.model';
 import { AppService } from 'src/app/services/app.service';
 import { FirestoreDataService } from 'src/app/services/firestore-data.service';
+import { HighlightSpanKind } from 'typescript';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent implements OnInit, OnDestroy {
 
   shopitems;
   currentUser;
   currentUserSubscription;
+  purchaseSubscription;
   parent;
   itemNames = [];
   itemImages = [];
   itemPrices = [];
+  purchaes: Purchase[] = []
+  objects: string[] = []
 
   constructor(private afs: FirestoreDataService, private app: AppService) { }
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
+    this.purchaseSubscription.unsubscribe();
+  }
 
   async ngOnInit(){
     this.app.myHeader("Shop")
+
     this.currentUserSubscription = this.afs.currentUserStatus.subscribe(async data => {
       this.currentUser = data
       if(data != null){
+
+        if (this.currentUser.role == 3) {
+          this.purchaseSubscription = this.afs.purchasesStatus.subscribe((data) => {
+            this.purchaes = data;
+            this.objects = [];
+            if (this.purchaes != null) {
+              this.purchaes.forEach((purchase) => {
+                this.objects.push(purchase.objectID);
+              });
+              console.log(this.objects)
+            }
+          });
+        }
         this.parent = (await this.afs.getUserPerID(this.currentUser.parent))[0]
         if(parent != null){
           this.initialize()
@@ -55,7 +79,10 @@ export class ShopComponent implements OnInit {
     }
   console.log("namen:")
   console.log(this.itemImages)
-  
+  console.log(this.itemNames)
+  console.log(this.objects.indexOf(this.itemNames[0]))
+  console.log(this.objects.indexOf(this.itemNames[1]))
+
   
   }
 
